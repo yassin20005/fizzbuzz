039b9898896010d5bc57a4d1e605b72d0f966a5a
yassin20005 <yassin344552@gmail.com>
2025-10-22 10:27:17 +0100

    fizz and normal number test

--- test/fizzbuzz_test.py
+++ test/fizzbuzz_test.py
@@ -2,3 +2,11 @@ from src.fizzbuzz import fizzbuzz
 
 def fizzbuzz_with_3():
     assert fizzbuzz(3) == "Fizz"
+def fizzbuzz_with_6():
+    assert fizzbuzz(6) == "Fizz"
+def fizzbuzz_with_9():
+    assert fizzbuzz(9) == "Fizz"
+def fizzbuzz_with_11():
+    assert fizzbuzz(11) == 11
+def fizzbuzz_with_1():
+    assert fizzbuzz(1) == 1
